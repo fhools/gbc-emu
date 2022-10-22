7f7610a70f99df44a2da4168a9f9919dd374cedf
fhools <fhools@gmail.com>
2022-10-22 13:27:00 -0700

    cpu: add LR35902Cpu, and drive disassembly of memory buffer

--- src/cpu.rs
+++ src/cpu.rs
@@ -7,11 +7,12 @@ impl<const V: u8> ConstEval<V> {
 
 
 /* This method of generating opcode handler is similar to tgbr's method 
- * instruction_opcodes will call a processing macro to generate 
- * handlers, the processing macro is run_macro
+ * use_z80_opcodes specifies a table of instructions and will call a 
+ * processing macro to generate handlers, the processing macro is run_macro
  *
  * the indexing macro will generate a opcode number to for each
- * table entry
+ * table entry it will then call the run_macro that will
+ * generate the actual handlers
  *
  * the format is one of the following:
  * MNE OP : N 
@@ -33,10 +34,14 @@ macro_rules! use_z80_opcodes {
     ($run_macro:ident) => { indexing!($run_macro  @start:
          /*       00               01              02              03             04              05              06              07             */ 
          /* 00 */ nop :1;          ld bc, nn :3;   ld (bc), a :1;  inc bc :1;     inc b :1;       dec b :1;       ld b, n :2;    rlca :1;
-         /* 01 */ ld (nn), sp :3;  add hl, bc :1;  ld a, (bc) :1;  dec bc :1;     inc c :1;       dec c :1;       ld c, n :2;    rrca :1;
-         /* 02 */ stop :2;         ld de, nn :3;   ld (de), a :1;  inc de :1;     inc d :1;       dec d :1;       ld d, n :2;    rla :1; 
-         /* 03 */ jr n :2;         add hl, de :1;  ld a, (de) :1;  dec de :1;     inc e :1;       dec e :1;       ld e, n :2;    rra :1;
-    );
+         /* 08 */ ld (nn), sp :3;  add hl, bc :1;  ld a, (bc) :1;  dec bc :1;     inc c :1;       dec c :1;       ld c, n :2;    rrca :1;
+         /* 10 */ stop :2;         ld de, nn :3;   ld (de), a :1;  inc de :1;     inc d :1;       dec d :1;       ld d, n :2;    rla :1; 
+         /* 18 */ jr n :2;         add hl, de :1;  ld a, (de) :1;  dec de :1;     inc e :1;       dec e :1;       ld e, n :2;    rra :1;
+         /* 20 */ jr nz, n :2;     ld hl, nn :3;   ld (hl+), a :1; inc hl :1;     inc h :1;       dec h :1;       ld h, n :2;    daa :1;
+         /* 28 */ jr z, n :2;      add hl, hl :1;  ld a, (hl+) :1; dec hl :1;     inc l :1;       dec l :1;       ld l, n :2;    cpl :1;
+         /* 30 */ 
+            
+    )
     }
 }
 
@@ -47,43 +52,31 @@ macro_rules! use_z80_opcodes {
 macro_rules! indexing {
     // start index counting at 0
     ($run_macro:ident @start: $($input:tt)*) =>  {
-        indexing!($run_macro @indexing: 0 => $($input)* @end);
+        indexing!($run_macro @indexing: 0 => $($input)* @end)
     };
 
     // mne : n 
     ($run_macro:ident @indexing: $ix:expr => $mne:ident :$n:expr; $($rest:tt)*) => {
-        indexing!($run_macro @indexing: $ix + 1 => $($rest)* $ix => $mne [], $n;);
+        indexing!($run_macro @indexing: $ix + 1 => $($rest)* $ix => $mne [], $n;)
     };
 
     // mne op : n
     ($run_macro:ident @indexing: $ix:expr => $mne:ident $opr:tt :$n:expr; $($rest:tt)*) => {
-        indexing!($run_macro @indexing: $ix + 1 => $($rest)* $ix => $mne [$opr], $n;);
+        indexing!($run_macro @indexing: $ix + 1 => $($rest)* $ix => $mne [$opr], $n;)
     };
 
     // mne op, op : n
     ($run_macro:ident @indexing: $ix:expr => $mne:ident $dst:tt , $src:tt :$n:expr; $($rest:tt)*) => {
-        indexing!($run_macro @indexing: $ix + 1 => $($rest)* $ix => $mne [$dst, $src], $n;);
+        indexing!($run_macro @indexing: $ix + 1 => $($rest)* $ix => $mne [$dst, $src], $n;)
     };
 
     // end of list
     ($run_macro:ident @indexing: $_:expr => @end $($ix:expr => $mne:ident  $opr:tt, $n:expr; )*) => {
-        $run_macro!($($ix => $mne $opr, $n;)*);
+        $run_macro!($($ix => $mne $opr, $n;)*)
     };
 }
 
-macro_rules! gen_z80_handlers {
-    ($($ix:expr => $mne:tt $opr:tt, $n:expr;)*) => {
-        pub fn do_it(opc: u8) {
-                use crate::cpu::ConstEval;
-            match opc {
-                $( ConstEval::<{$ix}>::VALUE => {
-                    gen_a_z80_handler!($mne $opr $n)
-                }, )*
-                _ => { panic!("problem!"); }
-            }
-        }
-    }
-}
+
 
 macro_rules! gen_a_z80_handler {
     ($mne:ident [] $n:expr) => { 
@@ -100,61 +93,200 @@ macro_rules! gen_a_z80_handler {
 }
 
 macro_rules! gen_mne {
-    (nop $n:expr) => {
-        println!("{} bytes: {}", $n, "nop")
-    };
+    (nop $n:expr) => { {
+        println!("{} bytes: {}", $n, "nop");
+        ($n as u8)
+    }};
 
-    (ld $dst:tt $src:tt $n:expr) => {
-        println!("{} bytes: ld {} = {}", $n, stringify!($dst), stringify!($src))
-    };
+    (ld $dst:tt $src:tt $n:expr) => {{
+        let _srcval = load!($src);
+        println!("{} bytes: ld {} = {}", $n, stringify!($dst), _srcval);
+        ($n as u8)
+    }};
 
-    (inc $opr:tt $n:expr) => {
-        println!("{} bytes: inc {}", $n, stringify!($opr))
-    };
+    (inc $opr:tt $n:expr) => {{
+        println!("{} bytes: inc {}", $n, stringify!($opr));
+        ($n as u8)
+    }};
 
-    (dec $opr:tt $n:expr) => {
-        println!("{} bytes: inc {}", $n, stringify!($opr))
-    };
+    (dec $opr:tt $n:expr) => {{
+        println!("{} bytes: inc {}", $n, stringify!($opr));
+        ($n as u8)
+    }};
 
-    (add $dst:tt $src:tt $n:expr) => {
-        println!("{} bytes: add {} = {} + {}", $n, stringify!($dst), stringify!($dst), stringify!(src))
-    };
+    (add $dst:tt $src:tt $n:expr) => {{
+        println!("{} bytes: add {} = {} + {}", $n, stringify!($dst), stringify!($dst), stringify!(src));
+        ($n as u8)
+    }};
 
 
-    (jr n $n:expr) => {
-        println!("{} bytes: jr n", $n)
-    };
+    (jr n $n:expr) => {{
+        println!("{} bytes: jr n", $n);
+        ($n as u8)
+    }};
+
+    (jr $op1:tt $op2:tt $n:expr) => {{
+        println!("{} bytes: jr {}, {}", $n, stringify!($op1), stringify!($op2));
+        ($n as u8)
+    }};
     
-    (stop $n:expr) => {
-        println!("{} bytes: stop", $n)
-    };
+    (stop $n:expr) => {{
+        println!("{} bytes: stop", $n);
+        ($n as u8)
+    }};
 
-    (rra $n:expr)  => {
-        println!("{} bytes: r__a", $n)
-    };
+    (rra $n:expr)  => {{
+        println!("{} bytes: rra", $n);
+        ($n as u8)
+    }};
     
-    (rla $n:expr)  => {
-        println!("{} bytes: r__a", $n)
-    };
+    (rla $n:expr)  => {{
+        println!("{} bytes: rla", $n);
+        ($n as u8)
+    }};
     
-    (rlca $n:expr)  => {
-        println!("{} bytes: r__a", $n)
-    };
+    (rlca $n:expr)  => {{
+        println!("{} bytes: rlca", $n);
+        ($n as u8)
+    }};
     
-    (rrca $n:expr)  => {
-        println!("{} bytes: r__a", $n)
-    };
+    (rrca $n:expr)  => {{
+        println!("{} bytes: rrca", $n);
+        ($n as u8)
+    }};
+    
+    (cpl $n:expr) => {{
+        println!("{} bytes: cpl", $n);
+        ($n as u8)
+    }};
+
+    (daa $n:expr) => {{
+        println!("{} bytes: daa", $n);
+        ($n as u8)
+    }};
+
 }
 
-mod mystuff {
-use_z80_opcodes!(gen_z80_handlers);
+
+pub struct LR35902Cpu<'a> {
+    mem: &'a [u8],
+    pc: u16,
 }
+
+
+impl<'a> LR35902Cpu<'a> {
+
+    fn new(start_pc: u16, mem: &'a[u8]) -> Self {
+        LR35902Cpu {
+            mem,
+            pc: start_pc
+        }
+    }
+    // TODO: This should be changed to read from the bus object instead of directly from mem
+    fn read8(&self, addr: u16) -> u8 {
+        self.mem[addr as usize]
+    }
+
+
+    fn pc(&self) -> u16 {
+        self.pc
+    }
+
+    fn set_pc(&mut self, pc: u16) {
+        self.pc = pc;
+    }
+
+    /*
+     * disassemble opcode, using pc to fetch the rest of 
+     * the instruction operands 
+     *
+     * returns the length of the instruction, this will
+     * be used to drive the rest of the disassembly
+     */
+    fn disasm(&self, opc: u8) -> u8 {
+        /* generates a function do_it(opc: u8) that for now just decodes
+         * opcode and prints out the disassembled instruction
+         */
+        macro_rules! gen_z80_handlers {
+            ($($ix:expr => $mne:tt $opr:tt, $n:expr;)*) => {
+                match opc {
+                    $( ConstEval::<{$ix}>::VALUE => {
+                        gen_a_z80_handler!($mne $opr $n)
+                    }, )*
+                    _=> { panic!("problem!"); }
+                }
+            }
+        }
+       
+        /* note: this macro must be declared inside disasm() because it 
+         * references self. If we wanted this macro to be declared at the
+         * module level, we would have to pass in self as an identifier
+         * to this macro
+         */
+        macro_rules! load {
+            (n) => {{
+                self.read8(self.pc.wrapping_add(1))
+            }};
+
+            (nn) => {{
+                let lo = self.read8(self.pc.wrapping_add(1));
+                let hi = self.read8(self.pc.wrapping_add(2));
+                lo as u16 | ((hi as u16) << 8)
+            }};
+
+            ((b)) => {{
+                todo!();
+               0 
+            }};
+
+            ((bc)) => {{
+                todo!();
+               0 
+            }};
+
+            ((de)) => {{
+                todo!();
+               0 
+            }};
+
+            (sp)  => {{
+                todo!();
+                0
+            }};
+
+            (a) => {{
+                todo!();
+                0
+            }};
+            ((hl+)) => {{
+                todo!();
+                0
+            }};
+
+        }
+
+        use_z80_opcodes!(gen_z80_handlers)
+
+    }
+
+}
+
+
+
                 
 #[test]
-fn try_opcode() {
-    use crate::cpu::mystuff::do_it;
-    for i in (0..=10) {
-        do_it(i as u8);
+fn test_disasm() {
+    use crate::cpu::LR35902Cpu;
+    let code_buffer = [0x00, // nop
+                       0x06, // ld b, n (0xff) 
+                       0xff,
+                       0x03]; // inc b
+    let mut pc = 0;
+    let mut cpu = LR35902Cpu::new(pc, &code_buffer);
+    while cpu.pc() < (code_buffer.len() as u16) {
+        let opcode = cpu.read8(cpu.pc()); 
+        let oplen = cpu.disasm(opcode) as usize;
+        cpu.set_pc(cpu.pc() + (oplen as u16));
     }
 }
 
